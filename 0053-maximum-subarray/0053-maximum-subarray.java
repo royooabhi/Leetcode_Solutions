f2bb9f40722baf0064baa721c4ea3e2658b930ac
ABHIJEET KUMAR ROY <ABHIJEETKUMARROY88@GMAIL.COM>
2026-09-06 23:27:34 +0530

    Time: 1 ms (99.94%), Space: 77.3 MB (32.46%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,13 +3,21 @@ class Solution {
         int currentSum = nums[0];
         int maxSum = nums[0];
 
-        for (int i = 1; i<nums.length;i++){
-            
-            currentSum = Math.max(nums[i],currentSum + nums[i]);
+        for (int i = 1; i < nums.length; i++) {
+            if (currentSum + nums[i] > nums[i]) {
+                currentSum = currentSum + nums[i];
+            } else {
+                currentSum = nums[i];
+            }
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
 
-            maxSum = Math.max(maxSum,currentSum);
+            //     currentSum = Math.max(nums[i],currentSum + nums[i]);
+
+            //     maxSum = Math.max(maxSum,currentSum);
         }
         return maxSum;
-        
+
     }
 }
