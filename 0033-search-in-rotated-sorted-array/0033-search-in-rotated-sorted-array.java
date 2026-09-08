3ffe24241709c18ef680774412e4364beb020d0b
ABHIJEET KUMAR ROY <ABHIJEETKUMARROY88@GMAIL.COM>
2026-09-09 00:10:34 +0530

    Time: 1 ms (1.84%), Space: 43.9 MB (46.99%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,44 +1,50 @@
 class Solution {
     public int search(int[] nums, int target) {
 
-        int left = 0;
-        int right = nums.length - 1;
+        // int left = 0;
+        // int right = nums.length - 1;
 
-        while (left <= right) {
+        // while (left <= right) {
 
-            int mid = left + (right - left) / 2;
+        //     int mid = left + (right - left) / 2;
 
-            // Target found
-            if (nums[mid] == target) {
-                return mid;
-            }
+        //     // Target found
+        //     if (nums[mid] == target) {
+        //         return mid;
+        //     }
 
-            // Left half is sorted
-            if (nums[left] <= nums[mid]) {
+        //     // Left half is sorted
+        //     if (nums[left] <= nums[mid]) {
 
-                // Target lies in left sorted half
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
-                } 
-                else {
-                    left = mid + 1;
-                }
+        //         // Target lies in left sorted half
+        //         if (nums[left] <= target && target < nums[mid]) {
+        //             right = mid - 1;
+        //         } 
+        //         else {
+        //             left = mid + 1;
+        //         }
 
-            }
+        //     }
 
-            // Right half is sorted
-            else {
+        //     // Right half is sorted
+        //     else {
 
-                // Target lies in right sorted half
-                if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
-                } 
-                else {
-                    right = mid - 1;
-                }
-            }
-        }
+        //         // Target lies in right sorted half
+        //         if (nums[mid] < target && target <= nums[right]) {
+        //             left = mid + 1;
+        //         } 
+        //         else {
+        //             right = mid - 1;
+        //         }
+        //     }
+        // }
 
-        return -1;
+        // return -1;
+        ArrayList<Integer> a=new ArrayList<>();
+ for(int i:nums)
+ {
+    a.add(i);
+ }
+ return a.indexOf(target);
     }
 }
